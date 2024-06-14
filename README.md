2bc1d2410e3e0cf7b4d639967c2ad2b03452cea9
denniszielke <11569044+denniszielke@users.noreply.github.com>
2024-06-14 17:21:59 +0200

    New picture

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-# ai agent for document manipulation
-This repository host the document agent
+# Secure Dynamic Session for AI Agent code execution
+This project demonstrates how to use the Azure Dynamic Session Service to safely execute AI generated code to interact with files in an isolated session.
+The code in the repository is using the following technologies:
+- Azure OpenAI (as LLM to process user requests and translate them dynamically into Python code)
+- Langchain (as as LLM orchestrator to leverage OpenAI and handle tool execution)
+- Streamlit (as frontend technology to host the user interface and upload/download files)
+- Azure Container Apps (as container runtime for the streamlit application)
+- Azure Dynamic Sessions (as host for executing LLM generated python code in a secure session)
+- Azure Developer CLI (as means to deploy all resources in the project to Azure)
 
-Regions that this deployment can be executed:
-- eastus
+Here is a high level diagram on how these components work together:
+
+![# reference an image in markdown](architecture.png)
+
+This architecture pattern might be relevant for you if any of the following requirements are applicable for your scenario:
+- You need to execute AI generated code on the fly
+- You need to isolate tool execution for different user sessions 
+- You need to provide dedicated compute/filesystem resources for a user session
+- You need to scale out compute sessions in less than a second
+- You need to prevent generated code to connect to the network
+- You need to provide a secured filesystem per session 
 
 ## Quickstart
 
+The recommended way to execute the deployment is to fork the repo and then start a GitHub Codespace because there you will have all the tools to deploy the resources, test the code and try out the whole story end-to-end.
+The project resources can be deployed into the following Azure regions:
+- eastus
+
 ```
+
 echo "log into azure dev cli - only once"
 azd auth login
 
@@ -14,10 +35,9 @@ echo "provisioning all the resources with the azure dev cli"
 azd up
 
 echo "get and set the value for AZURE_ENV_NAME"
-azd env get-values | grep AZURE_ENV_NAME
-source <(azd env get-values)
+source <(azd env get-values | grep AZURE_ENV_NAME)
 
-echo "building and deploying the agent for phase 1"
+echo "building and deploying the streamlit user interface"
 bash ./azd-hooks/deploy.sh web $AZURE_ENV_NAME
 
 ```
@@ -28,3 +48,5 @@ look into /mnt/data/phase3_facts.json which contains a list of facts about movie
 
 
 python -m streamlit run app.py --server.port=8000
+
+Create an in-game screenshot of Guybrush Threepwood using his smartphone relaxing in a lounger between two palm trees, on a small island on the side is a roboter which is programming on a laptop with the other hand, wide shot, shallow depth of field, in the style of Monkey Island. The robot is listening to Guybrush and is making changes to large excel files according to his ideas. Make the resolion very low 200 pixels height and 400 pixels width.
--- src/web/app.py
+++ src/web/app.py
@@ -13,11 +13,20 @@ from langchain.agents import AgentExecutor, create_react_agent, load_tools
 from langchain_community.callbacks.streamlit import (
     StreamlitCallbackHandler,
 )
+import random
 
 dotenv.load_dotenv()
 
-st.title("💬 AI file manipluation agent")
-st.caption("🚀 A Streamlit chatbot powered by Langchain, Azure OpenAI and Azure Container Apps")
+st.title("💬 AI code execution agent")
+st.caption("🚀 A Bot that can run dynamically generated code powered by Langchain, Azure OpenAI and Azure Container Apps")
+
+def get_session_id() -> str:
+    id = random.randint(0, 1000000)
+    return "00000000-0000-0000-0000-" + str(id).zfill(12)
+
+if "session_id" not in st.session_state:
+    st.session_state["session_id"] = get_session_id()
+    print("started new session: " + st.session_state["session_id"])
 
 llm: AzureChatOpenAI = None
 if "AZURE_OPENAI_API_KEY" in os.environ:
@@ -43,7 +52,7 @@ else:
 
 
 pool_management_endpoint = os.getenv("POOL_MANAGEMENT_ENDPOINT")
-repl = SessionsPythonREPLTool(pool_management_endpoint=pool_management_endpoint)
+repl = SessionsPythonREPLTool(pool_management_endpoint=pool_management_endpoint, session_id=st.session_state["session_id"])
 
 @tool
 def download_file(filepath: str) -> str:
@@ -54,7 +63,7 @@ def download_file(filepath: str) -> str:
     filepath = filepath.replace(" ", "")
     filename = os.path.basename(filepath)
     print("Downloading file:-", filename + "-")
-    f = repl.download_file(remote_file_path=filename, local_file_path=filename)
+    f = repl.download_file(remote_file_path=filename)
     
     st.download_button("Download file", f, file_name=filename)
     return "Sending file: " + filename 
@@ -63,8 +72,9 @@ def download_file(filepath: str) -> str:
 tools = [repl, download_file]
 
 if 'file_path' not in st.session_state:
-    st.session_state['file_path'] = 'No file available'
+    st.session_state['file_path'] = 'No file found'
 
+st.write("You are running in session: " + st.session_state["session_id"])
 
 promptString = """Answer the following questions as best you can. You have access to the following tools:
 
@@ -77,10 +87,6 @@ Question: the original input question you must answer
 Thought: you should always think about what to do
 
 Action: the action to take, can be one of [{tool_names}]
-If there is a reference to a file use the following file in that location: {file_path}
-Your work directory for all file operations is /mnt/data/
-Only use the download file tool if the user explicitly asks to get the file itself. Do not read the contents of the file to the user.
-After the download_file tool has been used do not read the file itself and STOP doing any further actions.
 
 Action Input: the input to the action
 
@@ -93,6 +99,10 @@ STOP if you have observed the answer to the original input question
 Thought: I now know the final answer
 
 Final Answer: the final answer to the original input question
+
+If there is a reference to a file use the following file in that location: {file_path}
+Your work directory for all file operations is /mnt/data/
+Only use the download file tool if the user explicitly asks to download the file itself. Otherwise describe to the user what is in the file.
 
 Begin!
 
@@ -117,7 +127,6 @@ if uploaded_file is not None:
     st.session_state['file_path'] = '/mnt/data/' + uploaded_file.name
 
 if prompt := st.chat_input():
-
 
     st.chat_message("user").write(prompt)
 
